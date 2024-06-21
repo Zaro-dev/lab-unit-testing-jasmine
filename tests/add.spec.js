882ce8bdc9e839e1dd8b8dd5a637cd2d9a6c0c3d
Mario <mariolazaroredolar@gmail.com>
2024-06-21 21:10:46 +0200

    primer test

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,12 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("should return undefined if any of the arguments is not a number", () => {
+        expect(add(NaN,NaN)).toEqual(undefined);
+        expect(add(1,NaN)).toEqual(undefined);
+        expect(add(NaN,2)).toEqual(undefined)
+      });
     });
   });
   
